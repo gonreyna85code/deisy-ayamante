594b9c80b87a00409dbc8f3ec44c9fc4b144eaf2
gonreyna85@gmail.com <gonreyna85@gmail.com>
2024-06-07 00:43:02 -0300

    english start button fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './screens/home';
 import Section1 from './screens/section1';
 import Section2 from './screens/section2';
