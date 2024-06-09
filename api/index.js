69dedf9fc47a41b478893452bc9f3f8f3f212238
gonreyna85@gmail.com <gonreyna85@gmail.com>
2024-06-08 23:30:10 -0300

    env config updated

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const PORT = process.env.PORT || 4000;
