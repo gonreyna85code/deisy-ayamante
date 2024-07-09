11a64e0358ffca267f3725896e047650d6448e10
gonreyna85@gmail.com <gonreyna85@gmail.com>
2024-07-09 19:03:34 -0300

    spotify sample fix

--- src/screens/Meditations.jsx
+++ src/screens/Meditations.jsx
@@ -10,7 +10,7 @@ const Meditations = () => {
                     <h2>{t('meditations.title')}</h2>
                     <p>{t('meditations.subtitle')}</p>
                 </header>
-                <iframe title="Conscientes" style={{ "borderRadius": "12px" }} src="https://open.spotify.com/embed/playlist/2iRWhqVHYvcbDMmHzofe4u?utm_source=generator" width="100%" height="600px" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe style={{ "borderRadius": "12px" }} src="https://open.spotify.com/embed/playlist/2iRWhqVHYvcbDMmHzofe4u?utm_source=generator" width="100%" height="600px" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         </section>
     );
